f83d4563036e9b8d6f80f1d3b4df7bf5109f4fa3
Xhokabash <xhokabash@gmail.com>
2023-08-27 01:27:57 +0200

    add libraryy

--- index.js
+++ index.js
@@ -2,62 +2,52 @@ const btn = document.querySelector(".btn");
 const form = document.querySelector(".form");
 const submit = document.querySelector(".submit");
 
-
 const myLibrary = [];
-
-
-
 
 // display form
 
 const displayForm = function () {
-    form.setAttribute("id", "form");
+  form.setAttribute("id", "form");
 };
 btn.addEventListener("click", displayForm);
-
 
 // constructor to add new books
 
 function Book(title, author, pages) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
 }
 
 // adding new books
 
-
 function addBookToLibrary() {
-    const newTitle = document.querySelector(".title").value;
-    const newAuthor = document.querySelector(".author").value;
-    const newPages = document.querySelector(".pages").value;
-    const newBook = new Book(newTitle, newAuthor, newPages);
-    myLibrary.push(newBook);
+  const newTitle = document.querySelector(".title").value;
+  const newAuthor = document.querySelector(".author").value;
+  const newPages = document.querySelector(".pages").value;
+  const newBook = new Book(newTitle, newAuthor, newPages);
+  myLibrary.push(newBook);
 }
-console.log(myLibrary)
 
-document.querySelector(".form").addEventListener("submit", function(event){
-    event.preventDefault();
-    addBookToLibrary();
-    showBook();
+document.querySelector(".form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  addBookToLibrary();
+  showBook();
 });
 
-    function showBook(){
-        const screen = document.querySelector(".screen");
-        screen.innerHTML = "";
-        
-        myLibrary.forEach(element =>{
-            let book = element;
-            let bookCards = document.createElement("div");
-            console.log(book.title);
-            bookCards.innerHTML = `<h4>Title: ${book.title}</h4>
+function showBook() {
+  const screen = document.querySelector(".screen");
+  screen.innerHTML = "";
+
+  myLibrary.forEach((element) => {
+    let book = element;
+    let bookCards = document.createElement("div");
+    bookCards.innerHTML = `<h4>Title: ${book.title}</h4>
             <h4>Author: ${book.author}</h4>
-            <h4>Pages: ${book.pages}</h4>`
+            <h4>Pages: ${book.pages}</h4>`;
 
-    screen.appendChild(bookCards)
-    }
-
-    )}
-
+    screen.appendChild(bookCards);
+  });
+}
 
 // renderBook();
